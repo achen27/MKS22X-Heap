d8b167bc50df9318dc6dad755addc2ade138e6f0
Amanda Chen <achen01@stuy.edu>
2019-05-01 14:31:09 -0400

    fixed pushDown when child and parent values are equal

--- MyHeap.java
+++ MyHeap.java
@@ -20,7 +20,7 @@ public class MyHeap {
           data[index * 2 + 1] = data[index];
           data[index] = temp;
           index = index * 2 + 1; //update index
-        } else if (child2 > child1 && child2 > data[index]){ //second child is larger than first child and is greater than parent
+        } else if (child2 >= child1 && child2 > data[index]){ //second child is larger than first child and is greater than parent
           int temp = child2; //swap values
           data[index * 2 + 2] = data[index];
           data[index] = temp;
@@ -74,20 +74,55 @@ public class MyHeap {
   public static void heapsort(int[]data){
     heapify(data); //changes data into heap
     for(int i = data.length-1; i >= 0; i--){ //looping from back to front
-      int largest = data[0]; //stores largest number
-      data[0] = data[i]; //swaps largest number to proper position
-      data[i] = largest;
+      int swap = data[i]; //stores largest number
+      data[i] = data[0]; //swaps largest number to proper position
+      data[0] = swap;
       pushDown(data,i,0); //pushes down swapped value
     }
   }
 
-  public static void main(String args[]){
+  /*public static void main(String args[]){
     int[] data = {3,5,32,446,7,4784,7,43,4,3436,6564,532,4,7,786,2};
     System.out.println(HeapHelp.toString(data));
     //pushDown(data,data.length,0);
     //pushUp(data,8);
     heapsort(data);
     System.out.println(HeapHelp.toString(data));
+  }*/
+
+  public static void main(String[]args){
+  System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+  int[]MAX_LIST = {1000000000,500,10};
+  for(int MAX : MAX_LIST){
+    for(int size = 31250; size < 2000001; size*=2){
+      long qtime=0;
+      long btime=0;
+      //average of 5 sorts.
+      for(int trial = 0 ; trial <=5; trial++){
+        int []data1 = new int[size];
+        int []data2 = new int[size];
+        for(int i = 0; i < data1.length; i++){
+          data1[i] = (int)(Math.random()*MAX);
+          data2[i] = data1[i];
+        }
+        long t1,t2;
+        t1 = System.currentTimeMillis();
+        MyHeap.heapsort(data2);
+        t2 = System.currentTimeMillis();
+        qtime += t2 - t1;
+        t1 = System.currentTimeMillis();
+        Arrays.sort(data1);
+        t2 = System.currentTimeMillis();
+        btime+= t2 - t1;
+        if(!Arrays.equals(data1,data2)){
+          System.out.println("FAIL TO SORT!");
+          System.exit(0);
+        }
+      }
+      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+    }
+    System.out.println();
   }
+}
 
 }
